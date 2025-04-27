8a297e552cc97f69a151bcc1c7085c5d7e7e5551
Kelsey <kelsey.nsterner@gmail.com>
2025-04-27 12:04:53 -0400

    fixed firebase config

--- src/app/firebase.js
+++ src/app/firebase.js
@@ -5,12 +5,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: "swe6050-822c6.firebaseapp.com",
   projectId: "swe6050-822c6",
   storageBucket: "swe6050-822c6.firebasestorage.app",
   messagingSenderId: "620592048949",
-  appId: "1:620592048949:web:ae867dd57f1264236c7a7c"
+  appId: "1:620592048949:web:01759715c1b308756c7a7c"
 };
 
 // Initialize Firebase
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,6 +14,7 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  console.log('API Key:', process.env.NEXT_PUBLIC_FIREBASE_API_KEY);
   return (
     <html lang="en">
       <body>
