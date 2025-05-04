8d5c12c19ab73ee24c09b2378a331ad8be972abe
Kelsey <kelsey.nsterner@gmail.com>
2025-05-04 11:40:43 -0400

    Change NavBar truelinks type

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -10,7 +10,7 @@ import { useRouter } from "next/navigation";
 type NavbarProps = {
   title: string;
   color?: string;
-  links?: Array<{ label: string; href: string, function?: string }> | null;
+  links?: Array<{ label: string; href: string, function?: string }>;
 };
 
 export default function Navbar({
